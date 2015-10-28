5d44576c560272cf154a1bde7ec55eba09fd376d
rocyuan <yuanzp@jpush.cn>
2015-10-28 21:26:09 +0800

    ftp ...

--- src/main/java/com/rocyuan/idea/FTPRouteBuilder.java
+++ src/main/java/com/rocyuan/idea/FTPRouteBuilder.java
@@ -17,6 +17,13 @@ public class FTPRouteBuilder extends RouteBuilder {
 //    	from("file:d:/temp/inbox/?delay=30000").to("file:d:/temp/outbox");  //本地文件互传
 //    	from("ftp://127.0.0.1/wamp/tmp?username=wangwt&password=123456&delay=30000").to("file:d:/temp/outbox");  //下载
     	from("file:d:/temp/outbox").to("ftp://127.0.0.1/wamp/tmp2?username=wangwt&password=123456");  //上传
+
+
+
+
+
+//        第三种方法，设置FTPClient的编码，这个终于OK了，关键就在最后的红字，ftpClient.controlEncoding=gb2312
+//        String uri = "ftp://username@10.10.XX4.122/中文目录?password=xxxx&ftpClient.controlEncoding=gb2312";
     	
     	
     }
---
+++ src/main/java/com/rocyuan/idea/FileCopierFTPMain.java
@@ -0,0 +1,61 @@
+package com.rocyuan.idea;
+
+import javax.annotation.processing.Processor;
+import org.apache.camel.CamelContext;
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.impl.DefaultCamelContext;
+import org.apache.camel.Processor;
+import org.apache.camel.Exchange;
+import java.io.File;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import org.apache.camel.component.file.GenericFile;
+
+/**
+ * Created by rocyuan on 2015/10/28.
+ */
+public class FileCopierFTPMain {
+
+        public static void main(String args[]) throws Exception
+        {
+            int i = 0;
+            CamelContext context = new DefaultCamelContext();
+
+            context.addRoutes(new RouteBuilder()
+            {
+                public void configure() {
+
+                    from("ftp://192.168.230.1:21?username=mauro")
+                            .choice()
+                            .when(header("CamelFileName").endsWith(".xml"))
+                            .process(new Processor() {
+                                public void process(Exchange exchange) throws Exception {
+
+                                    System.out.println("Copying file XML: "
+                                            + exchange.getIn().getHeader("CamelFileName"));
+                                    System.out.println(exchange.getIn().getBody());
+                                }
+                            })
+                            .to("file:C:/DataOUT")//.setHeader(Exchange.FILE_NAME,simple("${file:name.	noext}.old"))
+                            .when(header("CamelFileName").endsWith(".txt"))
+                            .process(new Processor() {
+                                public void process(Exchange exchange) throws Exception {
+
+                                    System.out.println("Copying file txt: "
+                                            + exchange.getIn().getHeader("CamelFileName"));
+                                    System.out.println(exchange.getIn().getBody());
+                                }
+                            })
+                            .to("file:C:/DataIN"); //.to("ftp://192.168.230.1:21?username=mauro");*/
+				/*prova aggiunto commento
+				Aggiunto un ulteriore commento*/
+                }
+            });
+
+            context.start();
+
+            Thread.sleep(10000);
+
+            context.stop();
+        }
+}
