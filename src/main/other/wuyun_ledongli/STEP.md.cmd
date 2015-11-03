3d1a224ef8f180c39fc71ff6494efa5953ad0193
rocyuan <1045138965@qq.com>
2015-11-03 23:46:25 +0800

    pic

--- src/main/other/wuyun_ledongli/STEP.md.cmd
+++ src/main/other/wuyun_ledongli/STEP.md.cmd
@@ -5,3 +5,7 @@
 ## 3.抓取手机数据 ，在 app设置里 上传你的数据（用 花瓶 或 其他的道具）
 
 ## 3.改 脚本你的参数为 你自己的。。
+
+每天微信运动排行榜第一名 你值得拥有
+
+![mahua](http://7xj6si.com1.z0.glb.clouddn.com/Screenshot_2015-11-03-21-31-35_com.tencent.mm.png)
